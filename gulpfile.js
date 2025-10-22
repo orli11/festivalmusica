32921bf1a5fd4393ce54d8e22ca80ef017c5f85c
Orlando Javier <oj.flores118@gmail.com>
2025-10-22 16:54:43 -0600

    Agregando google fonts

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
 
+// Compile SCSS to CSS
 export function css( done ) {
     src('src/scss/app.scss')
         .pipe( sass() )
@@ -11,6 +12,8 @@ export function css( done ) {
     done()
 }
 
+// Watch for changes in SCSS files and run the css task
 export function dev() {
-    watch('src/scss/app.scss', css)
+    watch('src/scss/app.scss', css) 
 }
+

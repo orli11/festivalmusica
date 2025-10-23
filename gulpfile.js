1f5d25eafc8a17e23a07310c377f41c8558e19e9
Orlando Javier <oj.flores118@gmail.com>
2025-10-23 14:53:46 -0600

    Añadiendo estilos, moviendo variables a carpetas, añadiendo watch a archivos sass

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ const sass = gulpSass(dartSass)
 // Compile SCSS to CSS
 export function css( done ) {
     src('src/scss/app.scss')
-        .pipe( sass() )
+        .pipe( sass().on('error', sass.logError) ) //Aplica SASS, si hay un error lo muestra en consola
         .pipe( dest('build/css') )
     done()
 }
 
 // Watch for changes in SCSS files and run the css task
 export function dev() {
-    watch('src/scss/app.scss', css) 
+    watch('src/scss/**/*.scss', css) // watch(archivos a observar, tarea a ejecutar)
 }
 

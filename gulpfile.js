8815aed5944cd204c0164b8e4296708ae6a8492a
Orlando Javier <oj.flores118@gmail.com>
2026-01-02 16:30:11 -0600

    Añadiendo JS al proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
-import { src, dest, watch } from 'gulp'
+import { src, dest, watch, series } from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
+
+export function js(done) { // Tarea para copiar los archivos JS
+    src('src/js/app.js') //Identifica y selecciona los archivos JS
+        .pipe( dest('build/js') ) //Los copia a la carpeta de destino   
+    done()  
+}
 
 // Compile SCSS to CSS
 export function css( done ) {
@@ -15,5 +21,7 @@ export function css( done ) {
 // Watch for changes in SCSS files and run the css task
 export function dev() {
     watch('src/scss/**/*.scss', css) // watch(archivos a observar, tarea a ejecutar)
+    watch('src/js/**/*.js', js) // watch(archivos a observar, tarea a ejecutar)
 }
 
+export default series( js, css, dev) // series ejecuta las tareas en orden 
